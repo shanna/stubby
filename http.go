cb0d9d4032c01126184db881d142a762793c2049
Shane Hanna <shane.hanna@gmail.com>
2017-01-25 23:50:45 +0000

    Add JWT auth. Real redirects.

--- http.go
+++ http.go
@@ -1,40 +1,41 @@
 package stubby
 
-// TODO: JWT protect meta data and creation.
-// TODO: Eh. Redirect instead of dumping out JSON debugging.
+// TODO: Break admin into its own handler so auth can be removed?
+// TODO: Logging.
 
 import (
 	"net/http"
 
+	"github.com/goware/jwtauth"
 	"github.com/pressly/chi"
 	"github.com/pressly/chi/render"
-	"github.com/y0ssar1an/q"
 )
 
-func Handler(shortener *Shortener) http.Handler {
+func Handler(shortener *Shortener, secret []byte) http.Handler {
 	router := chi.NewRouter()
 
 	router.Get("/:id", func(w http.ResponseWriter, r *http.Request) {
-		q.Q(chi.URLParam(r, "id"))
 		record, err := shortener.Get(chi.URLParam(r, "id"))
 		if err != nil {
-			render.JSON(w, r, err)
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 			return
 		}
-		// TODO: Redirect instead of JSON.
-		render.JSON(w, r, record)
+		http.Redirect(w, r, record.URL, http.StatusPermanentRedirect)
 	})
 
-	router.Post("/", func(w http.ResponseWriter, r *http.Request) {
-		record, err := shortener.Create(r.PostFormValue("url"), nil)
-		if err != nil {
-			q.Q(err)
-			render.JSON(w, r, err)
-			return
-		}
-		render.JSON(w, r, record)
+	jwt := jwtauth.New("HS256", secret, nil)
+	router.Group(func(router chi.Router) {
+		router.Use(jwt.Verifier)
+		router.Use(jwtauth.Authenticator)
+
+		router.Post("/", func(w http.ResponseWriter, r *http.Request) {
+			record, err := shortener.Create(r.PostFormValue("url"), nil)
+			if err != nil {
+				render.JSON(w, r, err)
+				return
+			}
+			render.JSON(w, r, record)
+		})
 	})
-	// TODO: /s/:jwt for secure JWT authenticated redirects? The :id is embedded in the claim.
-	// TODO: /m/:jwt for secure JWT metadata for a url visits, json metadata, jwt claims etc.
 	return router
 }
