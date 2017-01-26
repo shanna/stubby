deacb1995bcf6e285dbddb1a6b0ec24e9bc7b388
Shane Hanna <shane.hanna@gmail.com>
2017-01-26 00:28:06 +0000

    Command flags and environment var config.

--- cmd/stubby/stubby.go
+++ cmd/stubby/stubby.go
@@ -1,6 +1,11 @@
 package main
 
+//go:generate go-bindata -prefix "public/" -pkg main -o public.go
+
 import (
+	"crypto/rand"
+	"flag"
+	"fmt"
 	"net/http"
 	"os"
 	"path/filepath"
@@ -12,17 +17,35 @@ import (
 	store "github.com/shanna/stubby/store/sqlite"
 )
 
-// TODO: flags.
 // TODO: errors.
 
+var config struct {
+	secret   []byte
+	listen   string
+	database string
+}
+
 func main() {
-	conn, err := store.Open("./stubby.db")
+	secret := os.Getenv("STUBBY_SECRET")
+	if secret != "" {
+		config.secret = []byte(secret)
+	} else {
+		fmt.Println("stubby warning: STUBBY_SECRET environment variable not set. Using random secret.")
+		config.secret = make([]byte, 32)
+		rand.Read(config.secret)
+	}
+
+	flag.StringVar(&config.listen, "listen", "localhost:3030", "listen address")
+	flag.StringVar(&config.database, "database", "./stubby.db", "sqlite database path")
+	flag.Parse()
+
+	conn, err := store.Open(config.database)
 	if err != nil {
 		panic(err) // TODO:
 	}
 	defer conn.Close()
 
-	id, err := bjf.New([]byte("hard coded secret to be replaced"))
+	id, err := bjf.New(config.secret)
 	if err != nil {
 		panic(err) // TODO:
 	}
@@ -35,7 +58,7 @@ func main() {
 	router := chi.NewRouter()
 	router.Use(middleware.Recoverer)
 	router.Use(middleware.Logger)
-	router.Mount("/s", stubby.Handler(shortener))
+	router.Mount("/s", stubby.Handler(shortener, config.secret))
 
 	// TODO: Bundle public css/html into binary.
 	cwd, _ := os.Getwd()
